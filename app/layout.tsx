f968a5c4ce08073d1fc256115f4435d90e180860
Ashley Kim <ashleykim026@gmail.com>
2026-04-16 14:25:42 -0700

    update ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import './globals.css';
+import '../styles/globals.css';
 import { Navbar } from '@components/layout/Navbar';
 
 export const metadata: Metadata = {
